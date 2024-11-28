be4a3460f234ce2d1d06093605c8e873a21a9a8f
Irshath <irshath700@gmail.com>
2024-11-28 18:52:23 +0000

    Revert "temp roll your own dnd solution"
    This reverts commit 37dea0c72d39762377cc1694309f149fba21b98c.

--- frontend/src/features/tasks/tasks.tsx
+++ frontend/src/features/tasks/tasks.tsx
@@ -3,7 +3,6 @@ import { flushSync } from "react-dom";
 import { CreateCard } from "@/features/tasks/create-task";
 import { Button } from "@/components/ui/button";
 import { ColumnsQueryData } from "@/features/columns/queries";
-import { Task } from "@/features/tasks/task";
 
 export type Tasks = ColumnsQueryData["columns"][number]["tasks"];
 
@@ -14,7 +13,6 @@ export function Tasks(props: {
 }) {
   const [showAddTask, setShowAddTask] = useState(false);
   const listRef = useRef<HTMLUListElement>(null);
-  const sortedTasks = [...props.tasks].sort((a, b) => a.position - b.position);
 
   function scrollList() {
     listRef.current?.scrollTo({ top: listRef.current.scrollHeight });
@@ -35,23 +33,19 @@ export function Tasks(props: {
         ref={listRef}
         className="space-y-3 flex-grow overflow-y-auto px-2 min-h-0"
       >
-        {sortedTasks.map((task, i, arr) => {
-          const isLastEl = arr.length - 1 === i;
-          return (
-            <Task
-              task={task}
-              key={task.id}
-              ref={isLastEl ? lastTaskRef : undefined}
-              boardName={props.boardName}
-              previousPosition={
-                i == 0 ? arr[i]!.position - 1 : arr[i - 1]!.position
-              }
-              nextPosition={
-                isLastEl ? arr[i]!.position + 1 : arr[i + 1]!.position
-              }
-            />
-          );
-        })}
+        {[...props.tasks]
+          .sort((a, b) => a.position - b.position)
+          .map((task, i, arr) => {
+            return (
+              <li
+                key={task.id}
+                ref={arr.length - 1 === i ? lastTaskRef : undefined}
+                className="bg-background text-foreground p-2 rounded-md h-16"
+              >
+                {task.name}
+              </li>
+            );
+          })}
       </ul>
 
       <div className="shrink-0 mx-2 mt-3">
@@ -59,11 +53,7 @@ export function Tasks(props: {
           <CreateCard
             boardName={props.boardName}
             columnId={props.columnId}
-            nextPosition={
-              sortedTasks.length
-                ? sortedTasks[sortedTasks.length - 1]!.position + 1
-                : 1
-            }
+            nextPosition={props.tasks.length ?? 0}
             onComplete={() => {
               setShowAddTask(false);
             }}
