24e60988c96bb61544eb741a4ca123f94a8f0df7
Irshath <irshath700@gmail.com>
2024-12-15 23:46:06 +0530

    use radix colors

--- frontend/src/components/column.tsx
+++ frontend/src/components/column.tsx
@@ -8,7 +8,7 @@ export const ColumnWrapper = forwardRef<
   return (
     <div
       className={cn(
-        "w-80 py-2 bg-muted rounded-md space-y-3 shrink-0 border max-h-full flex flex-col h-fit",
+        "w-72 py-2 shadow-md bg-gray-3 rounded-md space-y-3 shrink-0 border max-h-full flex flex-col h-fit",
         className
       )}
       ref={ref}
