9acb00da25036548bd1d0ec9e4d7006d332e08af
Irshath <irshath700@gmail.com>
2024-12-16 23:07:47 +0530

    minor fixes

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,9 +1,21 @@
 import { api } from "@/lib/openapi-react-query";
 import { queryClient } from "@/lib/query-client";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AppContextProvider } from "@/state/app-state";
+import { lazy } from "react";
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        }))
+      );
 
 export const Route = createRootRoute({
   component: RootComponent,
