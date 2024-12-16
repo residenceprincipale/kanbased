c05f79616ca2842d7581cad26db1b47c04c0b40c
Irshath <irshath700@gmail.com>
2024-12-16 21:40:33 +0530

    fix build stuff

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -3,9 +3,8 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./src/db/schema/index.ts",
   dialect: "postgresql",
-  out: "./src/db/migrations",
+  out: "./drizzle",
   dbCredentials: {
-    // eslint-disable-next-line node/no-process-env
     url: process.env.DATABASE_URL,
   },
   casing: "snake_case",
