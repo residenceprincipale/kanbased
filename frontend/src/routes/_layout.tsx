badf0bda5e00f41806a63cf86cca5d4072d6d403
Irshath <irshath700@gmail.com>
2025-01-28 23:10:46 +0530

    bug: temp fix scrollbar appearing on board detail page

--- frontend/src/routes/_layout.tsx
+++ frontend/src/routes/_layout.tsx
@@ -1,6 +1,7 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { cn } from "@/lib/utils";
+import { createFileRoute, Outlet, useLocation } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_layout")({
   component: RouteComponent,
@@ -10,14 +11,32 @@ function RouteComponent() {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className="w-full pb-2 px-2 h-svh flex flex-col">
+      <MainLayoutWrapper>
         <div className="shrink-0 sticky top-0 z-10 bg-background py-2">
           <SidebarTrigger />
         </div>
-        <div className="flex-1">
+        <div className="flex-1 min-h-0">
           <Outlet />
         </div>
-      </main>
+      </MainLayoutWrapper>
     </SidebarProvider>
+  );
+}
+
+function MainLayoutWrapper(props: React.PropsWithChildren) {
+  const location = useLocation();
+  const isBoardDetailPage = /\/boards\/[^\/]+$/.test(location.pathname);
+
+  return (
+    <main
+      className={cn(
+        "w-full pb-2 px-2 h-svh flex flex-col",
+        // Temp fix to prevent scrollbar from appearing on board detail page,
+        // I couldn't figure out how to fix this properly yet.
+        isBoardDetailPage && "overflow-hidden"
+      )}
+    >
+      {props.children}
+    </main>
   );
 }
