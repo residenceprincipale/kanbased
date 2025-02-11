bd5da103a2e1fb4f2f0d344e3db994878d25a255
Irshath <irshath700@gmail.com>
2025-02-11 12:41:32 +0530

    get port from env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,16 @@
 import { serve } from "@hono/node-server";
-
 import app from "./app.js";
+import { env } from './env.js'
 
-const port = 5000;
+const port = Number(env.BE_PORT);
 
 serve(
   {
     fetch: app.fetch,
     port,
   },
-  (info) => {
-    // eslint-disable-next-line no-console
-    console.log(`Server is running on port ${info.port}`);
+  () => {
+    console.log(`Server is running on port ${port}`);
   },
 );
 
