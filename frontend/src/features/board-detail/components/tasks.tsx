3188b8f1a0fc0071c2874262a5e0e565f2544133
Irshath <irshath700@gmail.com>
2025-02-21 23:04:15 +0530

    hide x overflow on dnd

--- frontend/src/features/board-detail/components/tasks.tsx
+++ frontend/src/features/board-detail/components/tasks.tsx
@@ -74,7 +74,7 @@ export function Tasks(props: TasksProps) {
           return (
             <div
               className={cn(
-                "custom-scrollbar flex-grow min-h-0 overflow-y-auto"
+                "custom-scrollbar flex-grow min-h-0 overflow-y-auto overflow-x-hidden"
               )}
               {...droppableProvided.droppableProps}
               ref={containerCbRef}
