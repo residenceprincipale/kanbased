243fdebae4226a3d6168b0f9521e9f3913be4db1
Irshath <irshathv2@gmail.com>
2025-03-25 23:21:48 +0530

    Refactor layout component for improved structure and readability
    - Removed the `MainLayoutWrapper` component to simplify the layout structure.
    - Adjusted the main layout styling for better consistency and usability.
    - Cleaned up unused imports to enhance code clarity.

--- frontend/src/routes/_authenticated/_layout.tsx
+++ frontend/src/routes/_authenticated/_layout.tsx
@@ -1,13 +1,8 @@
 import { useMemo } from "react";
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { cn, getSidebarStateFromCookie } from "@/lib/utils";
-import {
-  createFileRoute,
-  linkOptions,
-  Outlet,
-  useLocation,
-} from "@tanstack/react-router";
+import { getSidebarStateFromCookie } from "@/lib/utils";
+import { createFileRoute, linkOptions, Outlet } from "@tanstack/react-router";
 import { BreadcrumbsData, TsrBreadcrumbs } from "@/components/tsr-breadcrumbs";
 import { BackButton } from "@/components/back-button";
 import { SidebarTrigger } from "@/components/ui/sidebar";
@@ -35,8 +30,8 @@ function RouteComponent() {
   return (
     <SidebarProvider defaultOpen={defaultSidebarState}>
       <AppSidebar />
-      <MainLayoutWrapper>
-        <div className="shrink-0 sticky top-0 z-[5] bg-background flex items-center gap-2">
+      <main className="w-full pb-2 h-svh flex flex-col">
+        <div className="shrink-0 sticky top-0 z-[5] bg-background flex items-center gap-2 py-1 px-2 border-b">
           <Tooltip delayDuration={300}>
             <TooltipTrigger asChild>
               <SidebarTrigger className="self-center" />
@@ -51,25 +46,7 @@ function RouteComponent() {
         <div className="flex-1 min-h-0">
           <Outlet />
         </div>
-      </MainLayoutWrapper>
+      </main>
     </SidebarProvider>
-  );
-}
-
-function MainLayoutWrapper(props: React.PropsWithChildren) {
-  const location = useLocation();
-  const isBoardDetailPage = /\/boards\/[^\/]+$/.test(location.pathname);
-
-  return (
-    <main
-      className={cn(
-        "w-full pb-2 px-2 h-svh flex flex-col",
-        // Temp fix to prevent scrollbar from appearing on board detail page,
-        // I couldn't figure out how to fix this properly yet.
-        isBoardDetailPage && "",
-      )}
-    >
-      {props.children}
-    </main>
   );
 }
