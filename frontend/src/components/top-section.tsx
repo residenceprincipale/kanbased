1912d3edd65766f824620ff183d7a030c0e57f62
Irshath <irshathv2@gmail.com>
2025-03-27 21:56:02 +0530

    Remove unused components and streamline layout structure
    - Deleted the `TabsList` and `TeamSwitcher` components to simplify the codebase.
    - Refactored the `TopSection` to integrate sidebar functionality and breadcrumbs, enhancing navigation.
    - Cleaned up imports and adjusted layout styling for improved readability and performance.

--- frontend/src/components/top-section.tsx
+++ frontend/src/components/top-section.tsx
@@ -1,80 +1,47 @@
-"use client";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { buttonVariants } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { routeMap } from "@/lib/constants";
-import { fetchAndCacheImage } from "@/lib/utils";
-import { Route } from "@/routes/__root";
-import { useAppContext } from "@/state/app-state";
-import { Link } from "@tanstack/react-router";
-import { House, User } from "lucide-react";
-import { useEffect, useState } from "react";
+import { TsrBreadcrumbs } from "@/components/tsr-breadcrumbs";
+import { SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
+import { WrappedTooltip } from "@/components/ui/tooltip";
+import { Separator } from "@/components/ui/separator";
 
 export function TopSection() {
-  const {
-    auth: { user },
-  } = Route.useRouteContext();
-  const { theme, updateTheme } = useAppContext();
-  const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
+  const { isMobile, state } = useSidebar();
 
-  useEffect(() => {
-    if (!user?.image) return;
-    fetchAndCacheImage(`${user.image}`, user.image).then((url) => {
-      if (!url) return;
-      setAvatarUrl(url);
-    });
-  }, [user?.image]);
+  const getSidebarWidth = () => {
+    if (isMobile) {
+      return "0px";
+    }
+
+    if (state === "collapsed") {
+      return "var(--sidebar-width-icon)";
+    }
+
+    return "var(--sidebar-width)";
+  };
 
   return (
-    <div className="flex items-center justify-between gap-2 py-1.5 px-4 w-screen fixed top-0 left-0 z-10 bg-background">
-      <div className="flex gap-6 items-center flex-1">
-        <Link
-          to={routeMap.home}
-          className={buttonVariants({
-            size: "icon",
-            variant: "outline",
-            className: "shrink-0",
-          })}
+    <div
+      className="sticky top-0 right-0 z-[5] flex items-center gap-2 justify-between py-1 px-2 border-b bg-background shrink-0"
+      style={{
+        left: getSidebarWidth(),
+        width: `calc(100vw - ${getSidebarWidth()})`,
+      }}
+    >
+      <div className="flex items-center gap-2">
+        <WrappedTooltip
+          tooltipProps={{ delayDuration: 300 }}
+          tooltipContentProps={{
+            side: "right",
+          }}
         >
-          <House />
-        </Link>
-        {/* Tab section */}
-        {/* <div className="flex-1">
-          <TabsList />
-        </div> */}
+          <SidebarTrigger className="self-center" />
+          <span>Toggle sidebar (⌘+B)</span>
+        </WrappedTooltip>
+
+        <Separator orientation="vertical" className="mr-2 h-4" />
+        <TsrBreadcrumbs />
       </div>
-      <div>
-        <DropdownMenu>
-          <DropdownMenuTrigger className="focus:outline-none">
-            <Avatar>
-              {avatarUrl ? (
-                <AvatarImage src={avatarUrl} alt={user!.displayName!} />
-              ) : (
-                <AvatarFallback className="border">
-                  <User />
-                </AvatarFallback>
-              )}
-            </Avatar>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56" align="end">
-            <DropdownMenuCheckboxItem
-              checked={theme === "dark"}
-              onCheckedChange={(checked) =>
-                updateTheme(checked ? "dark" : "light")
-              }
-            >
-              Dark mode
-            </DropdownMenuCheckboxItem>
-            <DropdownMenuItem>Log out</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
+
+      <div className="shrink-0">{/* TODO: Add CMD + K search bar */}</div>
     </div>
   );
 }
--- frontend/src/routes/_authenticated/_layout.tsx
+++ frontend/src/routes/_authenticated/_layout.tsx
@@ -3,16 +3,8 @@ import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { getSidebarStateFromCookie } from "@/lib/utils";
 import { createFileRoute, linkOptions, Outlet } from "@tanstack/react-router";
-import { BreadcrumbsData, TsrBreadcrumbs } from "@/components/tsr-breadcrumbs";
-import { BackButton } from "@/components/back-button";
-import { SidebarTrigger } from "@/components/ui/sidebar";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-import { Separator } from "@/components/ui/separator";
-
+import { BreadcrumbsData } from "@/components/tsr-breadcrumbs";
+import { TopSection } from "@/components/top-section";
 export const Route = createFileRoute("/_authenticated/_layout")({
   component: RouteComponent,
   loader: (): BreadcrumbsData => ({
@@ -31,19 +23,11 @@ function RouteComponent() {
   return (
     <SidebarProvider defaultOpen={defaultSidebarState}>
       <AppSidebar />
-      <main className="w-full pb-2 h-svh flex flex-col">
-        <div className="shrink-0 sticky top-0 z-[5] bg-background flex items-center gap-2 py-1 px-2 border-b">
-          <Tooltip delayDuration={300}>
-            <TooltipTrigger asChild>
-              <SidebarTrigger className="self-center" />
-            </TooltipTrigger>
-            <TooltipContent side="right">Toggle sidebar (⌘+B)</TooltipContent>
-          </Tooltip>
 
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <TsrBreadcrumbs />
-        </div>
-        <div className="flex-1 min-h-0">
+      <main className="flex flex-col h-svh">
+        <TopSection />
+
+        <div className="flex-1 min-h-0 h-full">
           <Outlet />
         </div>
       </main>
