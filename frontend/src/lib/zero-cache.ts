797a34f9ef3cd33d1ee2c4b093ab1e175c7cd18f
Irshath <irshathv2@gmail.com>
2025-04-12 16:32:42 +0530

    Restructure codebase

--- frontend/src/lib/zero-cache.ts
+++ frontend/src/lib/zero-cache.ts
@@ -1,14 +1,13 @@
 import { Zero } from "@rocicorp/zero";
 import { useZero } from "@rocicorp/zero/react";
-import { schema } from "shared";
-import { Schema } from "shared/src/schema";
+import { schema, type Schema } from "../../../backend/src/db/zero-schema";
 
 export function getZeroCache({ userId }: { userId: string }) {
   return new Zero({
     userID: userId,
     auth: () => undefined,
     server: import.meta.env.CLIENT_PUBLIC_SERVER,
-    schema: schema.schema,
+    schema: schema,
     kvStore: "idb",
   });
 }
