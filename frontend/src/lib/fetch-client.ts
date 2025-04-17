f677df7993c52d9af1517fa9b9e7daa64f0a94c6
Irshath <irshathv2@gmail.com>
2025-04-17 22:56:59 +0530

    update upload image bug

--- frontend/src/lib/fetch-client.ts
+++ frontend/src/lib/fetch-client.ts
@@ -1,13 +1,16 @@
 export async function get(url: string) {
-  return fetch(`${import.meta.env.CLIENT_MY_API_BASE_URL}/${url}`, {
+  return fetch(`${import.meta.env.CLIENT_MY_API_BASE_URL}${url}`, {
     credentials: "include",
   });
 }
 
 export function post(url: string, body: unknown) {
-  return fetch(`${import.meta.env.CLIENT_MY_API_BASE_URL}/${url}`, {
+  return fetch(`${import.meta.env.CLIENT_MY_API_BASE_URL}${url}`, {
     credentials: "include",
     method: "POST",
     body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
 }
