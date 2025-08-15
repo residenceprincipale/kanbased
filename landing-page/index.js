e0a4b0ba8fbad2b3d307e462d33800785608849f
Irshath <irshathv2@gmail.com>
2025-08-15 13:18:28 +0530

    fix nav bg styles while on footer

--- landing-page/index.js
+++ landing-page/index.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   const nav = document.querySelector(".nav");
+  const footerScrolledPosition = 5498;
 
   function onScroll() {
-    if (window.scrollY > 20) {
+    if (window.scrollY > footerScrolledPosition) {
+      nav.classList.add("footer-touched");
+      nav.classList.remove("scrolled");
+    } else if (window.scrollY > 20) {
       nav.classList.add("scrolled");
+      nav.classList.remove("footer-touched");
     } else {
       nav.classList.remove("scrolled");
+      nav.classList.remove("footer-touched");
     }
   }
 
